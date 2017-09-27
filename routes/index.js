ee40235b3344e135332f76e1b5fedc789f53fed4
vivek narayanan <vivek narayanan>
2017-09-28 00:45:13 +0530

    Mongodb URI added

--- routes/index.js
+++ routes/index.js
@@ -35,7 +35,6 @@ var routes = {
 exports = module.exports = function (app) {
 	// Views
 	app.get('/', routes.views.index);
-	app.get('/blog/:category?', routes.views.blog);
 	app.get('/blog/post/:post', routes.views.post);
 	app.get('/gallery', routes.views.gallery);
 
