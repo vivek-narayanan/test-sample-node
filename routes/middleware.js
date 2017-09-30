1b8c3992f792358ca6f7a6bb12c0ca3df89eaba1
vivek narayanan <vivek narayanan>
2017-09-30 22:42:06 +0530

    UI Change - Removed Categories

--- routes/middleware.js
+++ routes/middleware.js
@@ -20,7 +20,6 @@ var _ = require('lodash');
 exports.initLocals = function (req, res, next) {
 	res.locals.navLinks = [
 		{ label: 'Home', key: 'home', href: '/' },
-		{ label: 'Blog', key: 'blog', href: '/blog' },
 		{ label: 'Gallery', key: 'gallery', href: '/gallery' },
 	];
 	res.locals.user = req.user;
