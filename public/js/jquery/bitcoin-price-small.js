09a6649480b19c5460a9d14206b639ab07e9e924
Vivek <vvk.2013@gmail.com>
2017-11-12 01:20:00 +0530

    Seperated price from dropdown

---
+++ public/js/jquery/bitcoin-price-small.js
@@ -0,0 +1,23 @@
+
+//Script to get the Bitcoin Price 
+
+function bitSmall(){
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function(){
+        if(this.readyState == 4 && this.status == 200){
+            var myObj = JSON.parse(this.responseText);
+            var percent_change = myObj[0].percent_change_24h+'%';
+            var final;
+            if(myObj[0].percent_change_24h.charAt() == '-'){
+                final = percent_change.fontcolor("red");
+            }
+            else{
+                percent_change = '+'+ percent_change;
+                final = percent_change.fontcolor("green");															
+            }
+            document.getElementById("btcwdgt-price-small").innerHTML = '$'+myObj[0].price_usd+' '+final;	
+        }
+    };
+    xmlhttp.open("GET", "https://api.coinmarketcap.com/v1/ticker/bitcoin/", true);
+    xmlhttp.send();    
+}
