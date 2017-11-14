6143c6c1329aaadc72a2caa6f2d640149e2ddff1
Vivek <vvk.2013@gmail.com>
2017-11-14 23:31:38 +0530

    Removed the commented code

--- models/Post.js
+++ models/Post.js
@@ -15,7 +15,7 @@ Post.add({
 	title: { type: String, required: true },
 	state: { type: Types.Select, options: 'draft, published, archived', default: 'draft', index: true },
 	author: { type: Types.Relationship, ref: 'User', index: true },
-	publisher: { type: String },
+	postedby: { type: String },
 	publishedDate: { type: Types.Date, index: true, dependsOn: { state: 'published' } },
 	image: { type: Types.CloudinaryImage },
 	content: {
